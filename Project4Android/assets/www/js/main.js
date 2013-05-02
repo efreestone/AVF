d07210abc0a56ad56c76e7509716f6421530deb5
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2013-05-01 22:31:01 -0600

    Added weather geo mashup to android. working well but still styled with results

--- Project4Android/assets/www/js/main.js
+++ Project4Android/assets/www/js/main.js
@@ -44,6 +44,11 @@ $("#index").on("pageinit", function() {
 		$.mobile.changePage($("#api"));
 	});
 	
+	//Changepage function for mashups button
+	$("#seeMashups").on("click", function() {
+		$.mobile.changePage($("#mashup"));
+	});
+	
 	//Changepage function for native button
 	$("#seeNative").on("click", function() {
 		$.mobile.changePage($("#native"));
@@ -147,6 +152,61 @@ $("#twitter").on("pageinit", function() {
 		};
 	});
 }); //End of twitter pageinit
+
+$("#mashup").on("pageinit", function() {
+	
+	//Changepage function for weather button
+	$("#weatherBtn").on("click", function() {
+		$.mobile.changePage($("#weatherPage"));
+	});
+
+}); //End of mashup pageinit
+
+$("#weatherPage").on("pageinit", function() {
+	
+	var lat, lon;
+
+	$("#getWeather").on("click", function(){
+		//get current geolocation
+		var success = function(position){
+			var lat = position.coords.latitude;
+			var lon = position.coords.longitude;
+			
+			$("#weatherDisp").empty(); 
+			//Retrieve current weather based on current geo
+			$.ajax({
+				url : "http://api.wunderground.com/api/193b989b6db00f80/geolookup/conditions/q/" + lat + "," + lon + ".json",
+				dataType: "json",
+				success: function(data){
+				//console.log(data);
+				var location = data.current_observation.display_location.full;
+				var icon = data.current_observation.icon_url; 
+				var temp = data.current_observation.temp_f;
+				var weather = data.current_observation.weather;
+				var feel = data.current_observation.feelslike_f;
+				var time = data.current_observation.observation_time;
+				//Displat current weather in weatherDisp
+				$("#weatherDisp").append($("<div>")
+                				 .attr("class", "results")
+                				 .append($("<h1>" + location + "</h1>"))
+                				 .append($("<img src=" + icon + ">")
+                				 .attr("id", "weatherIcon"))
+                				 .append($("<h2>" + "Current Weather: " + weather + "</h2>"))
+                				 .append($("<h2>" + "Temp: " + temp + "&deg;F" + "</h2>"))
+                				 .append($("<h3>"+"Feels Like: " + feel + "&deg;F" + "</h3>"))
+                				 .append($("<h4>" + time + "</h3>"))
+                	);
+                }
+            })
+        }
+    
+    var error = function(error){
+	    alert(error.message)
+	}
+		navigator.geolocation.getCurrentPosition(success, error);
+	});
+	
+}); //End of weatherPage pageinit
 
 $("#native").on("pageinit", function() {
 
